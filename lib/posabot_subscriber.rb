2782f9771f95fc24f62029d464c6d908046c9343
voodoologic <headley.douglas@gmail.com>
2019-07-27 17:21:22 -0700

    exlusive false

--- lib/posabot_subscriber.rb
+++ lib/posabot_subscriber.rb
@@ -63,7 +63,7 @@ class PosabotSubscriber
 
   def broadcast_queue
     unless @broadcast_queue
-      @broadcast_queue  = channel.queue('posabot.broadcast', exclusive: true, name: 'broadcast')
+      @broadcast_queue  = channel.queue('posabot.broadcast', name: 'broadcast')
       @broadcast_queue.bind(broadcast_exchange)
     end
     @broadcast_queue
@@ -71,7 +71,7 @@ class PosabotSubscriber
 
   def inbox_queue
     unless @inbox_queue
-      @inbox_queue = channel.queue('posabot.inbox', exclusive: true, name: 'inbox')
+      @inbox_queue = channel.queue('posabot.inbox', name: 'inbox')
       @inbox_queue.bind(inbox_exchange)
     end
     @inbox_queue
